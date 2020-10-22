999c97c68ce898ccd9aab19c7b99796db12e181e
unknown <jacob.whitwell@students.plymouth.ac.uk>
2020-10-22 17:00:51 +0100

    Added name func

--- c-proj/c-proj/c-proj/Source.cpp
+++ c-proj/c-proj/c-proj/Source.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void Name()
+{
+	char charName[] = "Jay";
+	int charAge = 28;
+
+	printf("My name is %s.\n", charName);
+	printf("I am %d years old.\n", charAge);
+}
 
 int main()
 {
-	printf("Hello, World!");
+	printf("Hello, World!\n");
+
+	Name();
 }
